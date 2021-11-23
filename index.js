ac89ccea4d9ea7b1a0116795dc7ad0b4eab7a79e
unknown <yeasalehofficial2002@gmail.com>
2021-11-23 08:04:53 +0600

    final

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
-
+// uri
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y6afp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
